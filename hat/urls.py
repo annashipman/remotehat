9910631d3098f9b6d204adee4188a4e1d490593c
Anna Shipman <anna@annashipman.co.uk>
2020-05-02 20:34:44 +0100

    Add names and submit

--- hat/urls.py
+++ hat/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'hat'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_names', views.add_names, name='add_names'),
     path('submit_name', views.submit_name, name='submit_name'),
     path('retrieve_name', views.retrieve_random_name, name='retrieve_random_name'),
 ]
--- hat/views.py
+++ hat/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'hat/index.html')
 
+def add_names(request):
+    return render(request, "hat/add_names.html")
+
 def submit_name(request):
     famous_name = "Anna Shipman"
-    return HttpResponse("You've submitted: %s." % famous_name)
+    return render(request, 'hat/submit_name.html', {'famous_name': famous_name})
 
 def retrieve_random_name(request):
     response = "random name chosen by dice roll"
