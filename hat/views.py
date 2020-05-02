7832ae0433b3c350fa8fb71b4d152400b80721b7
Anna Shipman <anna@annashipman.co.uk>
2020-05-02 21:07:30 +0100

    Actually submit names to the database
    Doesn't handle any errors.

--- hat/views.py
+++ hat/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import FamousNames
 
 def index(request):
     return render(request, 'hat/index.html')
@@ -9,7 +10,16 @@ def add_names(request):
     return render(request, "hat/add_names.html")
 
 def submit_name(request):
-    famous_name = "Anna Shipman"
+    try:
+        famous_name = request.POST['famous_name']
+        f = FamousNames(name_text=famous_name)
+        f.save()
+    except:
+        # Not sure what error might be raised here
+        # Worth noting that it allows empty strings to be submitted
+        return render(request, 'hat/add_name.html', {
+            'error_message': "An error occurred",
+        })
     return render(request, 'hat/submit_name.html', {'famous_name': famous_name})
 
 def retrieve_random_name(request):
