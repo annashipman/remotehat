c7dee2fc732f4027fc1f98bdeeb85f616f7196a6
Anna Shipman <anna@annashipman.co.uk>
2020-06-10 22:03:17 +0100

    Apache on the Vagrant box could not find my module
    I'm really not happy with editing the wsgi file, it seems like something that shoudn't be touched, but I have been down the rabbithole many days with this, and this worked, so I am moving on with my life.

--- remotehat/wsgi.py
+++ remotehat/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/vagrant/remotehat')
 
 from django.core.wsgi import get_wsgi_application
 
