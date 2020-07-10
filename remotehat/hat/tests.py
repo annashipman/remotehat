375fa59c8fbb1a2c16b2f4511a59abd48f3b241c
Anna Shipman <anna@annashipman.co.uk>
2020-07-10 20:51:51 +0100

    Add test for add name
    Though I really don't quite understand the purpose of this. I still haven't quite grasped the structure Django is imposing here.

--- remotehat/hat/tests.py
+++ remotehat/hat/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .views import submit_name
+from .views import add_name, submit_name
 
 class ViewTests(TestCase):
+
+    def test_add_name(self):
+        response = self.client.get(reverse('hat:add_name'))
+        self.assertEqual(response.status_code, 200)
 
     def test_submit_name(self):
         response = self.client.get(reverse('hat:submit_name'))
