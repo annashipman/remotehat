ad43a24eca79882f69ab098fc1d4a9f896d4573a
Anna Shipman <anna@annashipman.co.uk>
2020-07-24 15:01:27 +0100

    Add test that submitting a name saves it to db
    I'm still slightly uncomfortable with what feels like mixed concerns; not sure if I've written it wrong or am just not yet familiar enough with the model.

--- remotehat/hat/tests.py
+++ remotehat/hat/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
+from .models import FamousNames
 from .views import add_name, submit_name
 
 class ViewTests(TestCase):
@@ -10,6 +11,12 @@ class ViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_submit_name(self):
-        response = self.client.get(reverse('hat:submit_name'))
+        testNames = FamousNames.objects.count()
+        self.assertEqual(testNames, 0)
+        c = Client()
+        response = c.post(reverse('hat:submit_name'), {'famous_name': 'Maya Angelou'})
         self.assertEqual(response.status_code, 200)
-
+        testNames = FamousNames.objects.count()
+        self.assertEqual(testNames, 1)
+        testFamousName = FamousNames.objects.get().name_text
+        self.assertEqual(testFamousName, "Maya Angelou")
