9fe6ccb51c4f5ba34492f506607b869807101d1f
Anna Shipman <anna@annashipman.co.uk>
2020-08-21 15:07:52 +0100

    Force pseudo-tty allocation
    This prevents the message `sudo: no tty present and no askpass program specified`.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 set -e
 
 rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress remotehat/hat $SSH_USER@$DEPLOY_HOST:$DEPLOY_PATH/remotehat/
-ssh -o StrictHostKeyChecking=no $SSH_USER@$DEPLOY_HOST sudo systemctl restart apache2
+ssh -t -o StrictHostKeyChecking=no $SSH_USER@$DEPLOY_HOST sudo systemctl restart apache2
